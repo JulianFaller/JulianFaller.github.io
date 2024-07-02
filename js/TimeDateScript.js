67c6172806c7880c85fa280de80d86de76b4eefc
JulianFaller <148760693+JulianFaller@users.noreply.github.com>
2024-07-02 10:44:41 +0200

    new Time/Date

---
+++ js/TimeDateScript.js
@@ -0,0 +1,22 @@
+function updateDateTime() {
+    const dateElement = document.getElementById('date');
+    const timeElement = document.getElementById('time');
+
+    const now = new Date();
+    
+    // Datum formatieren
+    const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = now.toLocaleDateString('de-DE', optionsDate);
+    
+    // Uhrzeit formatieren
+    const optionsTime = { hour: '2-digit', minute: '2-digit' };         // second: '2-digit'
+    const formattedTime = now.toLocaleTimeString('de-DE', optionsTime);
+    
+    dateElement.textContent = formattedDate;
+    timeElement.textContent = formattedTime;
+}
+
+// Aktualisiere Datum und Uhrzeit beim Laden der Seite
+updateDateTime();
+// Aktualisiere Datum und Uhrzeit jede Sekunde
+setInterval(updateDateTime, 1000);
